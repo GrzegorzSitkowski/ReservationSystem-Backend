963fd607db657a2a50a4f2e6f447348c72fb6027
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-07-12 18:58:20 +0200

    Configuration Swagger

--- ReservationSystem.Api/Program.cs
+++ ReservationSystem.Api/Program.cs
@@ -10,8 +10,24 @@ namespace ReservationSystem.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "ReservationSystem",
+                    Version = "v1",
+                    Description = "Web apllication to book of services.",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    {
+                        Name = "Grzegorz",
+                        Email = "sitkowskigrzegorz8@gmail.com"
+                    }
+                });
+            });
             builder.Services.AddOpenApi();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
@@ -19,6 +35,8 @@ namespace ReservationSystem.Api
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReservationSystem"));
             }
 
             app.UseHttpsRedirection();
