e6768cd0c687e61fa0758b1eb509c6cbc01a50e7
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-08-23 12:41:58 +0200

    Created ReservationDbContext

---
+++ ReservationSystem.Persistance/ReservationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReservationSystem.Persistance
+{
+    public class ReservationDbContext : DbContext
+    {
+        public ReservationDbContext(DbContextOptions<ReservationDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
