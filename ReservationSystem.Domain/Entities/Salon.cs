6ab4203b4dbb876c0c2e3e62cfa62e1eef0b067c
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-08-24 16:12:28 +0200

    Created entity of Salon.cs

---
+++ ReservationSystem.Domain/Entities/Salon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Domain.Entities
+{
+    public class Salon
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Email { get; set; }
+        public string? Description { get; set; }
+        public required Guid OwnerId { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+        public DateTime UpdatedAt { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
