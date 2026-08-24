4a8d7a7b9748e4278a24654063a238d800e160e9
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-08-24 16:07:12 +0200

    Added enum of UserRole.cs

--- ReservationSystem.Domain/Entities/User.cs
+++ ReservationSystem.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReservationSystem.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -13,7 +14,7 @@ namespace ReservationSystem.Domain.Entities
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Email { get; set; }
-        public required Role Roles { get; set; } //= new List<Role>();
+        public required UserRole Role { get; set; } //= new List<Role>();
         public required string PasswordHash { get; set; }
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now; 
         public DateTime UpdatedAt { get; set; }
---
+++ ReservationSystem.Domain/Enums/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Domain.Enums
+{
+    public enum UserRole
+    {
+        Customer,
+        Employee,
+        Owner,
+        Admin
+    }
+}
