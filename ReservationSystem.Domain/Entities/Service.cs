ed81faaf662b42c7d3a79ecfdc748908555131a5
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-08-24 16:20:09 +0200

    Created entity of Service.cs

---
+++ ReservationSystem.Domain/Entities/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Domain.Entities
+{
+    public class Service
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string? Description { get; private set; }
+        public decimal Price { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public Guid SalonId { get; private set; }
+        public bool IsActive { get; private set; } = true;
+    }
+}
