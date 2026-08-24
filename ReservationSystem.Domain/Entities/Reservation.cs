63428f887979b2dc67293786b4333ca4ada8b67e
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-08-24 16:33:46 +0200

    Created entity of Reservation.cs

---
+++ ReservationSystem.Domain/Entities/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Domain.Entities
+{
+    public class Reservation
+    {
+        public Guid Id { get; private set; }
+        public Guid CustomerId { get; private set; }
+        public Guid EmployeeId { get; private set; }
+        public Guid ServiceId { get; private set; }
+        public Guid SalonId { get; private set; }
+        public DateTimeOffset StartTime { get; private set; }
+        public DateTimeOffset EndTime { get; private set; }
+        public string? Notes { get; private set; }
+        public ReservationStatus Status { get; private set; } = ReservationStatus.Pending;
+    }
+}
