83208ab7b5d203638de41042958c62929e8269f8
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-08-24 16:14:46 +0200

    Added entity of Employee.cs

---
+++ ReservationSystem.Domain/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Domain.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public required Guid SalonId { get; set; }
+    }
+}
