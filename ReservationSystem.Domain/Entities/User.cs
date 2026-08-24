c3f8d42b4bcaf864d6f8d0b72c77ee886dacc9c6
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-08-24 16:29:24 +0200

    Fixed User.cs

--- ReservationSystem.Domain/Entities/User.cs
+++ ReservationSystem.Domain/Entities/User.cs
@@ -10,14 +10,14 @@ namespace ReservationSystem.Domain.Entities
 {
     public class User
     {
-        public Guid Id { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required string Email { get; set; }
-        public required UserRole Role { get; set; } //= new List<Role>();
-        public required string PasswordHash { get; set; }
-        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now; 
-        public DateTime UpdatedAt { get; set; }
-        public bool IsActive { get; set; } = true;
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public UserRole Role { get; private set; } 
+        public string PasswordHash { get; private set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow; 
+        public DateTimeOffset UpdatedAt { get; private set; }
+        public bool IsActive { get; private set; } = true;
     }
 }
