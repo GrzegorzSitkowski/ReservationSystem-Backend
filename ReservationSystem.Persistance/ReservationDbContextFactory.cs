146a9fc1d5c9666642968fbc3a123877fcfa2db4
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-09-20 13:54:21 +0200

    Created ReservationDbContextFactory.cs

---
+++ ReservationSystem.Persistance/ReservationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Persistance
+{
+    internal class ReservationDbContextFactory : DesignTimeDbContextFactoryBase<ReservationDbContext>
+    {
+        protected override ReservationDbContext CreateNewInstance(DbContextOptions<ReservationDbContext> options)
+        {
+            return new ReservationDbContext(options);
+        }
+    }
+}
