3f9068d4c91cd6f320f07ae22a3de47a373a0cf8
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2026-09-20 13:58:10 +0200

    Implementation DI in layer of Persistance

---
+++ ReservationSystem.Persistance/DepedencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Persistance
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ReservationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ReservationSystemDatabase")));
+            return services;
+        }
+}
